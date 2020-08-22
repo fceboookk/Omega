a361bf646d9905c556976d325c3fcbb16fa6406f
fceboookk <70056938+fceboookk@users.noreply.github.com>
2020-08-22 15:51:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyB2NHKrJqRPS3ByRpO8reub7qCnI9uCxUs",
+  authDomain: "azad-e82ee.firebaseapp.com",
+  databaseURL: "https://azad-e82ee.firebaseio.com",
+  projectId: "azad-e82ee",
+  storageBucket: "azad-e82ee.appspot.com",
+  messagingSenderId: "941485631876",
+  appId: "1:941485631876:web:7a6ec3ad4791f75fe855d1",
+  measurementId: "G-0Z5ZVJN9GL"
 };
 
      
